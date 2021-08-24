10d4f78700742cfc4c413a3ffef897632fe3d02d
Aryan Garg <aryangarg019@gmail.com>
2021-08-24 16:14:47 +0530

    Removed main from gcd.cpp (which was for testing)

--- gcd.cpp
+++ gcd.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
+#include "functions.h"
 
 using namespace std;
+
 
 int iterative_gcd(int x, int y){
 	// x is larger initially
@@ -19,22 +21,3 @@ int recursive_gcd(int x, int y){
 	return recursive_gcd(y, x%y);
 }
 
-int main(){
-	int x,y;
-	cout<<"*** GCD Calculator ***\n\n";
-	cout<<"x: ";
-	cin>>x;
-	cout<<"y: ";
-	cin>>y;
-
-	int inbuilt_gcd = __gcd(x,y),        // in-built function
-	    custom_gcd = iterative_gcd(x,y); // iterative gcd
-	int custom_gcd2 = recursive_gcd(x,y);
-	
-	cout<<"\nIn-built gcd of "<<x<<" and "<<y<<" is "<<inbuilt_gcd<<"\n";
-	cout<<"Custom iterative gcd of the same is "<<custom_gcd<<"\n";
-	cout<<"Custom recursive gcd of the same is "<<custom_gcd2<<"\n";
-	cout<<"\n*** END ***\n";
-
-	return 0;
-}

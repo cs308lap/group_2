4ed07e31a7f45f3b9b00d7f908bfc32119dc24c2
Aryan Garg <aryangarg019@gmail.com>
2021-08-24 15:56:47 +0530

    Added recursive custom gcd function

--- gcd.cpp
+++ gcd.cpp
@@ -12,6 +12,13 @@ int iterative_gcd(int x, int y){
 	return x;
 }
 
+
+int recursive_gcd(int x, int y){
+	if(y == 0) return x;
+ 	if(x < y) swap(x,y);
+	return recursive_gcd(y, x%y);
+}
+
 int main(){
 	int x,y;
 	cout<<"*** GCD Calculator ***\n\n";
@@ -22,9 +29,11 @@ int main(){
 
 	int inbuilt_gcd = __gcd(x,y),        // in-built function
 	    custom_gcd = iterative_gcd(x,y); // iterative gcd
+	int custom_gcd2 = recursive_gcd(x,y);
 	
 	cout<<"\nIn-built gcd of "<<x<<" and "<<y<<" is "<<inbuilt_gcd<<"\n";
 	cout<<"Custom iterative gcd of the same is "<<custom_gcd<<"\n";
+	cout<<"Custom recursive gcd of the same is "<<custom_gcd2<<"\n";
 	cout<<"\n*** END ***\n";
 
 	return 0;
